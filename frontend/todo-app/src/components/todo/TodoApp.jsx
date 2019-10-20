802314e3c87623427671c37c0a9b52ce87db41e3
Daniel Ho <s3718201@student.rmit.edu.au>
2019-10-20 16:28:53 +1100

    made frontend improvements

--- frontend/todo-app/src/components/todo/TodoApp.jsx
+++ frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -2,13 +2,11 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import AuthenticatedRoute from './AuthenticatedRoute.jsx'
 import LoginComponent from './LoginComponent.jsx'
-import EventPlannerComponent from './EventPlannerComponent.jsx'
 import ErrorComponent from './ErrorComponent.jsx'
 import HeaderComponent from './HeaderComponent.jsx'
 import FooterComponent from './FooterComponent.jsx'
 import LogoutComponent from './LogoutComponent.jsx'
 import WelcomeComponent from './WelcomeComponent.jsx'
-import TodoComponent from './TodoComponent.jsx'
 import RegisterComponent from './RegisterComponent.jsx'
 import CalendarComponent from './CalendarComponent.jsx'
 import MarketPlaceComponent from './MarketplaceComponent.jsx'
@@ -29,8 +27,6 @@ class TodoApp extends Component
                             <Route path="/login" component={LoginComponent} />
                             <Route path="/register" component={RegisterComponent} />
                             <AuthenticatedRoute path="/welcome/:name" component={WelcomeComponent} />
-                            <AuthenticatedRoute path="/todos/:id" component={TodoComponent} />
-                            <AuthenticatedRoute path="/eventplanner" component={EventPlannerComponent} />
                             <AuthenticatedRoute path="/calendar" component={CalendarComponent} />
                             <AuthenticatedRoute path="/marketplace" component={MarketPlaceComponent} />
                             <AuthenticatedRoute path="/additempanel/" component={AddMarketPlaceItemPanel} />
